ecc2d267865695bf81ecf5a99aca8391c1e7ad81
Nithesh <163291761+VC-Nithesh944@users.noreply.github.com>
2024-10-21 23:35:42 +0530

    Added code for checking Subsequency in java

---
+++ Data Structures/Top Interview 150/392] Is Subsequence.java
@@ -0,0 +1,17 @@
+//This has a time complexity of O(N) and runtime of 2ms
+//I get confused when to move pointers.
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+}
