632a4b8782f44a6eee0a02496de37911dcc5b67a
Nithesh <163291761+VC-Nithesh944@users.noreply.github.com>
2024-10-22 23:20:03 +0530

    Added code for checking Valid Parantheses in java

--- Data Structures/Top Interview 150/20] Valid Parantheses.java
+++ Data Structures/Top Interview 150/20] Valid Parantheses.java
@@ -1,1 +1,43 @@
+//This solution has a runtime of 2ms and Time complexity of O(N).
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() == 0) {
+            return true; // empty string is valid
+        }
 
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char symbol = s.charAt(i);
+
+            switch (symbol) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(symbol);
+                    break;
+
+                case ')':
+                case ']':
+                case '}':
+                    if (stack.isEmpty()) {
+                        return false; // there's no matching opening bracket
+                    }
+                    char next = stack.pop();
+                    if (!Matching(next, symbol)) {
+                        return false; // unmatched pair
+                    }
+                    break;
+
+                default:
+                    return false;
+            }
+        }
+        return stack.isEmpty(); // ensure no unmatched opening brackets remain
+    }
+
+    public boolean Matching(char open, char close) {
+        return (open == '(' && close == ')') ||
+               (open == '[' && close == ']') ||
+               (open == '{' && close == '}');
+    }
+}
